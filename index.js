31a0f9a833f57a5bf1f3433388bcda3cf01bf29d
rvelur <rvelasquez579@gmail.com>
2024-06-08 05:41:47 -0400

    Se agregan validaciones

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ app.listen(PORT, () => {
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
     try {
         const indexHTML = path.join(Public, 'index.html');
+        if (!await fs.stat(indexHTML).catch (() => false)){
+            console.log('Error en rescate del index.html')
+            return res.status(404).send('index.html no fue alcanzado')
+        }
         res.sendFile(indexHTML);
     } catch (error) {
         console.log(error);
@@ -27,6 +31,10 @@ app.get('/', (req, res) => {
 app.get('/deportes', async (req, res) => {
     try {
         const data = path.join(Public, 'deportes.json')
+        if (!await fs.stat(data).catch (() => false)){
+            console.log('Error en rescate del deportes.json')
+            return res.status(404).send('deportes.json no fue alcanzado')
+        }
         res.sendFile(data);
     } catch (error) {
         console.log(error);
@@ -35,8 +43,14 @@ app.get('/deportes', async (req, res) => {
 
 app.get('/agregar', async (req, res) => {
     try {
-        const {nombre, precio} = req.query
+        const {nombre, precio} = req.query;
+        if(!nombre || !precio) {
+            return res.status(400).send('Faltan Parametros')
+        }
         const data = path.join(Public, 'deportes.json');
+        if(!data) {
+            return res.status(400).send('Faltan data')
+        }
         const deportesJSON = await fs.readFile(data, 'utf8');
         const {deportes} = JSON.parse(deportesJSON)
         deportes.push({nombre, precio})
@@ -51,7 +65,13 @@ app.get('/agregar', async (req, res) => {
 app.get('/eliminar', async (req, res) => {
     try {
         const {nombre} = req.query;
+        if(!nombre){
+            return res.status(400).send('Faltan Parametros')
+        }
         const data = path.join(Public, 'deportes.json');
+        if(!data){
+            return res.status(400).send('Faltan data')
+        }
         const deportesJSON = await fs.readFile(data, 'utf8');
         let {deportes} = JSON.parse(deportesJSON);
         deportes = deportes.filter((item) => item.nombre !== nombre)
@@ -66,7 +86,13 @@ app.get('/eliminar', async (req, res) => {
 app.get('/editar', async (req, res) => {
     try {
         const {nombre, precio} = req.query;
+        if(!nombre || !precio) {
+            return res.status(400).send('Faltan Parametros')
+        }
         const data = path.join(Public, 'deportes.json');
+        if(!data) {
+            return res.status(400).send('Faltan data')
+        }
         const deportesJSON = await fs.readFile(data, 'utf8');
         let {deportes} = JSON.parse(deportesJSON);
         deportes = deportes.map((item) => {
